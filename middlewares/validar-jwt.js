c59057ad86ee2cbc9635b56ceb40d05e9fa32b43
Raspijabi <raspijabi@gmail.com>
2023-01-25 20:45:40 +0100

    first commit

--- middlewares/validar-jwt.js
+++ middlewares/validar-jwt.js
@@ -1,20 +1,20 @@
-const {response, request} = require('express');
+const { response, request } = require('express');
 const jwt = require('jsonwebtoken');
 
 const validarJWT = (req = request, res = response, next) => {
 
     const token = req.header('x-token');
 
-    if( !token ){
+    if (!token) {
         return res.status(401).json({
             ok: false,
             msg: 'No hay token en la petición'
         });
     }
-    
+
     try {
-        
-        const {uid, name} = jwt.verify(
+
+        const { uid, name } = jwt.verify(
             token,
             process.env.SECRET_JWT_SEED
         );
